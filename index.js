03f966e59befe26b8877285bd3640f89d93f3776
Daven Young <davenyoung@msn.com>
2022-04-05 16:28:57 -0600

    commit tree 33daa6a48cd66f7113da7fbe4a9e90a1003ba101

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const copyOfCats = [...cats,name];
+    return copyOfCats;
+}
+
+function prependCat(name){
+    let copyOfCats = [name,...cats];
+    return copyOfCats;
+}
+
+function removeLastCat(){
+    let copyOfCats = cats.slice(0, 2)
+    return copyOfCats;
+}
+
+function removeFirstCat(){
+    let copyOfCats = cats.slice(1)
+    return copyOfCats;
+}
